e1e3815a8db04095bd162c4607e00396a2cbe6b3
Tobey Yeh <bard.eyes@hotmail.com>
2017-12-02 17:35:09 +0800

    Edit

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using System.Net;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = from p in db.Product
+                       select p;
+            return View(data.Take(10));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //接收Post資料
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(Product data)
+        {
+            //經過驗正資料
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        public ActionResult Edit(int? id)
+        {
+            //找到id
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+
+                item.ProductName = data.ProductName;
+                item.Price = data.Price;
+                item.Stock = data.Stock;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        //狀態回傳訊息 HttpStatusCodeResult
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var product = db.Product.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(product);
+        //}
+
+
+        ////取得post資料，在做刪除
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    var product = db.Product.Find(id);
+        //    db.Product.Remove(product);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+
+
+
+        //取得post資料，在做刪除
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int? id)
+        {
+            var item = db.Product.Find(id);
+            db.OrderLine.RemoveRange(item.OrderLine.ToList());
+            db.Product.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        public ActionResult Details(int? id)
+        {
+            var product = db.Product.Find(id);
+            return View(product);
+        }
+    }
+}
---
+++ MVC5Course/ViewModel/ProductListVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.ViewModel
+{
+    public class ProductListVM
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(0, 999, ErrorMessage = "商品只能介於0~999之間")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:#}")]
+        public Nullable<decimal> Stock { get; set; }
+
+    }
+}
